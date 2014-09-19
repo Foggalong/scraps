d62c8a85d67123ba25a8a6cb0b9f96bacb3f8ce1
Joshua Fogg <joshua.h.fogg@gmail.com>
2014-09-19 23:30:51 +0100

    Create operators.js

---
+++ operators.js
@@ -0,0 +1,229 @@
+var x = 20 ;
+var y = 10 ;
+document.write("Variables");
+document.write("<br />");
+document.write("x = ", x);
+document.write("<br />");
+document.write("y = ", y);
+document.write("<br />");
+document.write("<br />");
+document.write("<br />");
+//Arithmatic Operators
+document.write("Arithmatic Operators");
+document.write("<br />");
+document.write("<br />");
+//Addition
+p = x + y;
+document.write("Addition");
+document.write("<br />");
+document.write(" x + y = ", p);
+document.write("<br />");
+document.write("<br />");
+//Subtraction
+q = x - y;
+document.write("Subtraction");
+document.write("<br />");
+document.write(" x - y = ", q);
+document.write("<br />");
+document.write("<br />");
+//Multiplication
+r = x * y;
+document.write("Multiplication");
+document.write("<br />");
+document.write(" x * y = ", r);
+document.write("<br />");
+document.write("<br />");
+//Division
+s = x / y;
+document.write("Division");
+document.write("<br />");
+document.write(" x / y = ", s);
+document.write("<br />");
+document.write("<br />");
+//Remainder
+x = 10 ;
+y = 6 ;
+z = x % y;
+document.write("Remainder");
+document.write("<br />");
+document.write(" x % y = ", z);
+document.write("<br />");
+document.write("<br />");
+document.write("<br />");
+document.write("<br />");
+//Increment and Decrement Operators
+document.write("Increment and Decrement Operators");
+document.write("<br />");
+document.write("<br />");
+//Increment
+document.write("Increment");
+document.write("<br />");
+x++;
+document.write(" x++ = ", x);
+document.write("  and  ");
+y++;
+document.write(" y++ = ", y);
+document.write("<br />");
+document.write("<br />");
+//Decrement
+document.write("Decrement");
+document.write("<br />");
+x = 20 ; 
+y = 10 ;
+x--;
+document.write(" x-- = ", x);
+document.write("  and  ");
+y--;
+document.write(" y-- = ", y);
+document.write("<br />");
+document.write("<br />");
+document.write("<br />");
+document.write("<br />");
+//Assignment operators
+x = 20;
+y = 10;
+document.write("Assignment operators ");
+document.write("<br />");
+document.write("<br />");
+//x+=y (x = x+y)
+document.write("x+=y (x = x+y)");
+document.write("<br />");
+x+=y;
+document.write(" x+=y is ",x );
+document.write("<br />");
+x = 20;
+y = 10;
+document.write("<br />");
+// x-=y (x = x-y)
+document.write("x+=y (x = x-y)");
+document.write("<br />");
+x-=y;
+document.write("x-=y is ", x);
+document.write("<br />");
+x = 20;
+y = 10;
+document.write("<br />");
+// x*=y (x = x*y)
+document.write("x*=y (x = x*y)");
+document.write("<br />");
+x*=y;
+document.write("x*=y is ", x);
+document.write("<br />");
+x = 20;
+y = 10;
+document.write("<br />");
+// x/=y (x = x/y)
+document.write("x/=y (x = x/y)");
+document.write("<br />");
+x/=y;
+document.write("x/=y is ", x);
+document.write("<br />");
+x = 20;
+y = 10;
+document.write("<br />");
+// x%=y (x = x%y)
+document.write("x%=y (x = x%y)");
+document.write("<br />");
+x%=y;
+document.write("x%=y is ", x);
+document.write("<br />");
+x = 20;
+y = 10;
+document.write("<br />");
+document.write("<br />");
+document.write("<br />");
+document.write("Comparison Operators");
+document.write("<br />");
+document.write("<br />");
+// Equal To
+document.write("Equal to");
+document.write("<br />");
+document.write("5=='5'");
+document.write("<br />");
+document.write("TRUE");
+document.write("<br />");
+document.write("<br />");
+// Equal To (type incluesive)
+document.write("Equal to (type inclusive)");
+document.write("<br />");
+document.write("5==='5'");
+document.write("<br />");
+document.write("FALSE");
+document.write("<br />");
+document.write("<br />");
+// Not Equal
+document.write("Not equal");
+document.write("<br />");
+document.write("x!=y");
+document.write("<br />");
+document.write("TRUE");
+document.write("<br />");
+document.write("<br />");
+// Greater Than
+document.write("Greater Than");
+document.write("<br />");
+document.write("x > y");
+document.write("<br />");
+if (x > y)
+{
+document.write(" TRUE");
+document.write("<br />");
+document.write("<br />");
+}
+else
+{
+document.write(" FALSE");
+document.write("<br />");
+document.write("<br />");
+}
+// Less Than
+document.write("Less Than");
+document.write("<br />");
+document.write("x < y");
+document.write("<br />");
+if (x < y)
+{
+document.write(" TRUE");
+document.write("<br />");
+document.write("<br />");
+}
+else
+{
+document.write(" FALSE");
+document.write("<br />");
+document.write("<br />");
+}
+// Greater Than or Equal to
+document.write("Greater Than or Equal to");
+document.write("<br />");
+document.write("x >= y");
+document.write("<br />");
+if (x >= y)
+{
+document.write(" TRUE");
+document.write("<br />");
+document.write("<br />");
+}
+else
+{
+document.write(" FALSE");
+document.write("<br />");
+document.write("<br />");
+}
+// Less Than or Equal to
+document.write("Less Than or Equal to");
+document.write("<br />");
+document.write("x <= y");
+document.write("<br />");
+if (x <= y)
+{
+document.write(" TRUE");
+document.write("<br />");
+document.write("<br />");
+}
+else
+{
+document.write(" FALSE");
+document.write("<br />");
+document.write("<br />");
+}
