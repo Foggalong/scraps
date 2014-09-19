92ec1c0fb48bdf41c85af817570501a0652e53a0
Joshua Fogg <joshua.h.fogg@gmail.com>
2014-09-19 23:20:42 +0100

    Create xkcd.py

---
+++ old/xkcd.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+import time
+print "STARTING WIFI AUTOCONFIG..."
+time.sleep(3)
+print "SEARCHING FOR WIFI..."
+time.sleep(5)
+print "FOUND NO OPEN NETWORKS"
+time.sleep(2)
+print "SEARCHING FOR WIFI..."
+time.sleep(4)
+print "FOUND SECURE NET SSID 'Sayles Family'"
+time.sleep(1)
+print "ESTABLISHING CONNECTION"
+time.sleep(3)
+print "TRYING COMMON PASSWORDS..."
+time.sleep(7)
+print "FAILED"
+time.sleep(2)
+print "CHECKING FOR WEP VULNERABILITIES..."
+time.sleep(7)
+print "NONE FOUND"
+time.sleep(3)
+print "CONNECTING VIA BLUETOOTH TO PHONE..."
+time.sleep(2)
+print "CONNECTION SUCCESSFUL"
+time.sleep(1)
+print "CALLING LOCAL SCHOOLS..."
+time.sleep(5)
+print "SAYLES CHILDREN LOCATED"
+time.sleep(1)
+print "NOTIFYING FEILD AGENTS..."
+time.sleep(15)
+print "CHILDREN AQUIRED"
+time.sleep(2)
+print "CALLING SAYLES PARENTS..."
+time.sleep(5)
+print "NEGOTIATING FOR WIFI PASSWORD..."
+print "[press crtl+c to cancel]"
+time.sleep(86400)
