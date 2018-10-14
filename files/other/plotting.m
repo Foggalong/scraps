917c610b6d69a0f8deef690fdf5768a184a2ce4b
Joshua H. Fogg <joshua.h.fogg@gmail.com>
2018-10-14 13:57:55 +0100

    Create plotting.m
    Code for creating two plots each with two graphs in octave

---
+++ files/other/plotting.m
@@ -0,0 +1,32 @@
+fdata = load('float_data.csv');
+ddata = load('double_data.csv');
+
+figure;
+
+% plot graphs for float data
+subplot(1,2,1)
+plot(fdata(1:21, 1), fdata(1:21, 3)); hold on
+plot(fdata(1:21, 1), fdata(1:21, 2)); hold off
+
+% decorations for float graphs
+ylim ([-0.25, 1.25]);
+title('Calculating terms using float variables')
+legend('Numerical Solution',
+       'Analytical Solution',
+       'location', 'southwest')
+xlabel('n'), ylabel('p_n')
+
+% plot graphs for double data
+subplot(1,2,2)
+plot(ddata(1:21, 1), ddata(1:21, 3)); hold on
+plot(ddata(1:21, 1), ddata(1:21, 2)); hold off
+
+% decorations for double graphs
+ylim ([-0.25, 1.25]);
+title('Calculating terms using double variables')
+legend('Numerical Solution',
+       'Analytical Solution',
+       'location', 'southwest')
+xlabel('n'), ylabel('p_n')
+
+saveas (1, 'figure1.png');
